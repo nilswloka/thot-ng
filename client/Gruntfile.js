e72d062dc42aa055a26609292599d32441bf9f62
Nils Wloka <nils.wloka@codecentric.de>
2014-08-24 00:16:13 +0200

    Added live reload and web server tasks

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -5,8 +5,37 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-bower-task');
   grunt.loadNpmTasks('grunt-wiredep');
   grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-parallel');
 
   grunt.initConfig({
+    parallel: {
+      server: {
+        options: {
+          grunt: true
+        },
+        tasks: ['connect:dev', 'watch:dev']
+      }
+    },
+    connect: {
+      dev: {
+        options: {
+          port: 8000,
+          keepalive: true,
+          base: 'app',
+          livereload: 35729
+        }
+      }
+    },
+    watch: {
+      dev: {
+        files: ['app/**/*.html', 'app/**/*.js', 'app/**/*.css', '!app/bower_components/**/*'],
+        options: {
+          livereload: true
+        }
+      }
+    },
     useminPrepare: {
       html: 'app/index.html'
     },
@@ -41,4 +70,7 @@ module.exports = function (grunt) {
     'usemin'
   ]);
 
+  grunt.registerTask('server', [
+    'parallel:server'
+  ]);
 };
